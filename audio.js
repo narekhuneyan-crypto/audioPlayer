57fc9d209b80f5c6e06747f2d341254f0894055c
marisahakyan9-cmd <mari.sahakyan9@tumo.org>
2026-07-13 16:19:56 +0400

    add js function

--- audio.js
+++ audio.js
@@ -15,7 +15,7 @@ let data = {
         "music/Vanotek-Promised Land.mp3",
         "music/Ennio Morricone-Le Vent,Le Cri.mp3"
     ],
-    poster:[
+    poster: [
         "https://media0.giphy.com/media/v1.Y2lkPTZjMDliOTUyc3d5ZHJiaTU2eThucWlub2VocmpiYXpnOHRneThxaGg1eDJmY210cSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Yl5nlnrtpQIrI1AfhD/giphy.gif",
         "https://media0.giphy.com/media/v1.Y2lkPTZjMDliOTUyc3d5ZHJiaTU2eThucWlub2VocmpiYXpnOHRneThxaGg1eDJmY210cSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Yl5nlnrtpQIrI1AfhD/giphy.gif",
         "https://media0.giphy.com/media/v1.Y2lkPTZjMDliOTUyc3d5ZHJiaTU2eThucWlub2VocmpiYXpnOHRneThxaGg1eDJmY210cSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Yl5nlnrtpQIrI1AfhD/giphy.gif",
@@ -24,4 +24,38 @@ let data = {
         "https://media2.giphy.com/media/v1.Y2lkPTZjMDliOTUyeWh6OTV1NnhtemN6djJ5eG51YzJkcjB1dGU1NXBscW8yamhzemU5eSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/tqfS3mgQU28ko/200w.gif"
 
     ]
+}
+
+
+let song = new Audio()
+let currentSong = 0
+
+window.onload = function () {
+    playSong()
+}
+
+function playSong() {
+    song.src = data.song[currentSong]
+
+    let songTitle = document.getElementById("songTitle")
+    songTitle.textContent = data.title[currentSong]
+
+    let img = document.getElementById("row1")
+    img.style.backgroundImage = "url (" + data.poster[currentSong] + ")"
+
+    let main = document.getElementById("main")
+    main.style.backgroundImage = "url (" + data.poster[currentSong] + ")"
+    song.play()
+
+}
+
+function playOrPauseSong() {
+    let play = document.getElementById("play")
+    if (song.paused) {
+        song.play()
+        play.src = "images/pause.png"
+    } else {
+        song.pause()
+        play.src = "images/play-button-arrowhead.png"
+    }
 }
